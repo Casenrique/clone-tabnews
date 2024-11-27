62b0fd8f5eed2986138c472d2b6875d55a0d153a
Carlos Henrique de Souza <80413608+Casenrique@users.noreply.github.com>
2024-11-27 22:56:03 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return<h1>"Todo caminho é o certo. Tudo poderia ter sido qualquer outra coisa e seria igualmente importante. 🛣️"</h1>
+  return (
+    <h1>
+      "Todo caminho é o certo. Tudo poderia ter sido qualquer outra coisa e
+      seria igualmente importante. 🛣️"
+    </h1>
+  );
 }
 
 export default Home;
